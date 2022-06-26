a136551d91c6180556898754bd73d83d24b1df2b
Askar Bisembaev <superdeveloper333@gmail.com>
2022-06-26 21:26:10 +0500

    no comments

---
+++ CategoryQueryProcessorTest/CategoryQueryTest.cs
@@ -0,0 +1,24 @@
+﻿using OMS.Data.Access.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using OMS.Queries.CategoryQuery;
+using Xunit;
+using FluentAssertions;
+
+public class CategoryQueryProcessorTest
+{
+	private CategoryQueryProcessor _query;
+	private Mock<IUnitOfWork> _uow;
+
+	[Fact]
+		public void GetShouldReturnAll()
+		{
+
+			var result = _query.Get().ToList();
+			result.Count.Should().Be(1);
+		}
+}
--- OMSWeb/Migrations/20220513145408_OMSCreate.Designer.cs
+++ OMSWeb/Migrations/20220513145408_OMSCreate.Designer.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OMSWeb;
+using OMSWeb.Data.Access.DAL;
 
 #nullable disable
 
