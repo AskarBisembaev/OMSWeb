a5fbda0267030cb1a3d15374356e816cb81bef14
Askar Bisembaev <superdeveloper333@gmail.com>
2022-07-20 19:40:09 +0500

    no comments

---
+++ src/OMS.Security/TokenAuthOption.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS.Security
+{
+    public class TokenAuthOption
+    {
+        public static string Audience { get; } = "ExpensesAudience";
+        public static string Issuer { get; } = "ExpensesIssuer";
+        public static RsaSecurityKey Key { get; } = new RsaSecurityKey(RSAKeyHelper.GenerateKey());
+        public static SigningCredentials SigningCredentials { get; } = new SigningCredentials(Key, SecurityAlgorithms.RsaSha256Signature);
+
+        public static TimeSpan ExpiresSpan { get; } = TimeSpan.FromMinutes(30);
+        public static string TokenType { get; } = "Bearer";
+    }
+}
